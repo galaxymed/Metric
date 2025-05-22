84ac6c1c12ea3aa715d54a312c6076c2c576321a
galaxymed <163075796+galaxymed@users.noreply.github.com>
2025-05-21 23:35:11 -0300

    Update tv.mjs

--- netlify/functions/tv.mjs
+++ netlify/functions/tv.mjs
@@ -1,33 +1,20 @@
 import fetch from "node-fetch";
-import extractStreamUrl from "./cheerio.js";  // ✅ Ahora busca dentro de la misma carpeta
 
 export async function handler(event) {
     try {
-        const urlFuente = "https://tv.invasivamedia.com/hls/live.m3u8"; // Página donde está el reproductor
-        
+        const urlFuente = "https://www.desdepylabs.com/external/tvaccionmov/telefuturo";
         const response = await fetch(urlFuente);
         const html = await response.text();
 
-        const streamingUrl = extractStreamUrl(html, "720p"); // Puedes cambiar la calidad: "1080p", "320p"
-
-        if (!streamingUrl) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ error: "No se encontró la URL m3u8 en el reproductor." })
-            };
-        }
-
         return {
             statusCode: 200,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ url: streamingUrl }),
+            headers: { "Content-Type": "text/html" },
+            body: html
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: "Error interno del servidor." })
+            body: JSON.stringify({ error: "Error al obtener HTML." })
         };
     }
-
-    console.log(html);
 }
